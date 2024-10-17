cb94985cf4f6a50289a7832bfbf6a2c872f434ec
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-17 11:36:13 +0000

    task 6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,18 @@
+-- Email validation to sent 
+-- TRIGGER AFTER UPDATE
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+	-- Get project id by project name
+	DECLARE project_id INT;
+	SELECT id INTO project_id FROM projects WHERE name = project_name;
+
+	-- If project_id is NULL (not found), create a new project
+	IF project_id IS NULL THEN
+		INSERT INTO projects (name) VALUES (project_name);
+		-- Get the new project ID
+		SET project_id = LAST_INSERT_ID();
+	END IF;
+	INSERT INTO corrections VALUES (user_id, project_id, score);
+END$$
+DELIMITER ;

c34e60cdcb2587346981b643eeabcce5113f5cbc
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-17 13:17:57 +0000

    fix task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,11 +8,11 @@ BEGIN
 	DECLARE student_count INT;
 	DECLARE student_average FLOAT DEFAULT 0;
 	SELECT SUM(score), COUNT(score) INTO student_sum, student_count FROM corrections
-	WHERE id = id; 
+	WHERE user_id = id; 
 	IF student_count <> 0 THEN
 		SET student_average = student_sum / student_count;
 	END IF;
 	UPDATE users
-	SET average_score = student_average WHERE user_id = id;
+	SET average_score = student_average WHERE users.id = id;
 END$$
 DELIMITER ;

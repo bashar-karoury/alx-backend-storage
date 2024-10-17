abf84dac2b7c7d02a60c1cc9eab992d71b984742
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-17 14:09:42 +0000

    fix task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,8 +2,7 @@
 -- creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month.
 CREATE VIEW need_meeting
 AS SELECT name
-FROM studnets
+FROM students
 WHERE score < 80 AND
-(last_meeting = NULL OR
-(DATEDIFF(last_meeting, CURDATE()) > 30));
-~                                                
+(last_meeting IS NULL OR
+(DATEDIFF(last_meeting, CURDATE()) > 30));                                     

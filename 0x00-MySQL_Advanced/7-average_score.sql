fe9f65f9f86cc82684fd21d48df2d548f4b2ce85
bashar-karoury <basharalkaroury@hotmail.com>
2024-10-17 12:59:19 +0000

    task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,18 @@
+-- Average score 
+-- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN id INT)
+BEGIN
+	-- Get project id by project name
+	DECLARE student_sum INT;
+	DECLARE student_count INT;
+	DECLARE student_average FLOAT DEFAULT 0;
+	SELECT SUM(score), COUNT(score) INTO student_sum, student_count FROM corrections
+	WHERE id = id; 
+	IF student_count <> 0 THEN
+		SET student_average = student_sum / student_count;
+	END IF;
+	UPDATE users
+	SET average_score = student_average WHERE user_id = id;
+END$$
+DELIMITER ;
